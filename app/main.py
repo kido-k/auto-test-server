a5f5e893e867f466e8b266c704e00fd97decd166
木戸浩太郎 <k.kido@bravesoft.co.jp>
2022-10-10 23:55:49 +0900

    m1で動くようにimageと設定を変更

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
 
+WAIT_TIME = 20
+
 if __name__ == '__main__':
+    print("start")
     # Selenium サーバーへ接続する。
     driver = webdriver.Remote(
         command_executor=os.environ["SELENIUM_URL"],
@@ -14,14 +17,38 @@ if __name__ == '__main__':
     )
     # 任意のHTMLの要素が特定の状態になるまで待つ
     # ドライバとタイムアウト値を指定
-    wait = WebDriverWait(driver, 10)
-    # Googleにアクセス
-    driver.get("https://google.com")
-    # "selenium"で検索
-    driver.find_element(By.NAME, "q").send_keys("selenium" + Keys.RETURN)
-    # 1件目の検索結果を取得(描画されるまで待機)
-    first_result = wait.until(
-        presence_of_element_located((By.CSS_SELECTOR, "h3")))
-    print(first_result.get_attribute("textContent"))
+    print("wait")
+    wait = WebDriverWait(driver, WAIT_TIME)
+    driver.set_window_size(1280, 1024)
+
+    # ------ ここから自動開始 ---------
+    # サイトにアクセスにアクセス
+    print("get")
+    driver.get("https://googole.com")
+    
+    # メールアドレスの入力
+    input_mail_address = driver.find_element(By.ID, "mail_address")
+    input_mail_address.send_keys("hoge@example.co.jp")
+    print("input mail")
+
+    # パスワードの入力
+    input_mail_password = driver.find_element(By.ID, "password")
+    input_mail_password.send_keys("password")
+    print("input password")
+
+    driver.save_screenshot("./image/login.png")
+
+    # ログインボタンを押下
+    button_login = driver.find_element(By.ID, "login_button")
+    button_login.click()
+    print("login button")
+
+    # ログイン後のページ遷移
+    wait.until(
+        presence_of_element_located((By.CLASS_NAME, "widget__wrap")))
+    driver.save_screenshot("./image/top.png")
+    
+    print("finish")
+    # ------ ここまで ---------
 
     driver.quit()
