d24814afc9a3a63d2eee870b9699856ff81f5591
木戸浩太郎 <k.kido@bravesoft.co.jp>
2022-10-10 13:13:25 +0900

    m1で動くようにimageと設定を変更

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
